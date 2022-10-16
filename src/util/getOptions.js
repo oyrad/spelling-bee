3d0a3ecd6f0bc408f2d75ef3d24ddd1b2616cb6a
oyrad <dsf997@gmail.com>
2022-10-16 10:50:07 +0200

    number of letters fix

--- src/util/getOptions.js
+++ src/util/getOptions.js
@@ -2,23 +2,23 @@ export const getOptions = difficulty => {
     let minLength;
     let maxLength;
     switch (difficulty) {
-        case 'beginner':
+        case "beginner":
             minLength = 3;
             maxLength = 6;
             break;
-        case 'easy':
+        case "easy":
             minLength = 7;
             maxLength = 10;
             break;
-        case 'medium':
+        case "medium":
             minLength = 11;
             maxLength = 14;
             break;
-        case 'hard':
+        case "hard":
             minLength = 15;
             maxLength = 19;
             break;
-        case 'veryHard':
+        case "veryHard":
             minLength = 20;
             maxLength = 50;
             break;
@@ -27,16 +27,18 @@ export const getOptions = difficulty => {
     }
 
     const options = {
-        method: 'GET',
-        url: 'https://wordsapiv1.p.rapidapi.com/words/',
+        method: "GET",
+        url: "https://wordsapiv1.p.rapidapi.com/words/",
         params: {
+            lettersMin: minLength,
+            lettersMax: maxLength,
             letterPattern: `^[a-zA-Z]{${minLength},${maxLength}}$`,
             random: true,
-            hasDetails: 'definitions',
+            hasDetails: "definitions",
         },
         headers: {
-            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-            'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+            "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+            "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
         },
     };
 
